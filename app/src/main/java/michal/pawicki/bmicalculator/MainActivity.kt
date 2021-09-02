b259930c954a7362457bec6f8b6430a080b2a176
MichalPawicki <michal_pawicki@op.pl>
2021-09-02 17:30:46 +0200

    first commit

---
+++ app/src/main/java/michal/pawicki/bmicalculator/MainActivity.kt
@@ -0,0 +1,23 @@
+package michal.pawicki.bmicalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import michal.pawicki.bmicalculator.databinding.ActivityMainBinding
+
+class MainActivity :AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.displayResult
+    }
+    /*
+    1. Przeczytać o viewBinding
+    2. Użyć viewBinding w tej aplikacji
+    3. Podpiąc akcje pod zdarzenie klikniecia na przycisk, wynikiem klikniecia ma byc wyswietlenie wartosci Bmi w polu tekstowym
+    4. Przeniesc ekran z Bmi do fragmentu
+     */
+}
