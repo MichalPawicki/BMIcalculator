6597d9e0b4d7bb774875a7141dbd46619821b600
MichalPawicki <michal_pawicki@op.pl>
2021-09-10 16:44:42 +0200

    corrects

--- app/src/main/java/michal/pawicki/bmicalculator/MainActivity.kt
+++ app/src/main/java/michal/pawicki/bmicalculator/MainActivity.kt
@@ -18,13 +18,17 @@ class MainActivity :AppCompatActivity() {
 
         binding.buttonCalculate.setOnClickListener{
             val height = binding.addHeight.text.toString().toFloat()
-            if (height == 0f) {
+            if (height == 0f ) {
                 binding.addHeight.error = "Podaj wartość większą od 0"
                 return@setOnClickListener
             }
             val weight = binding.addWeight.text.toString().toFloat()
+            if (weight == 0f) {
+                binding.addWeight.error = "Podaj wartość większą od 0"
+                return@setOnClickListener
+            }
             val bmi = calculateBmi(height, weight)
-            binding.displayResult.text = "Your BMI value: $bmi"
+            binding.displayResult.text = "Twoja wartość BMI wynosi: $bmi"
             binding.bmiInfo.text = bmiInfo(bmi)
         }
 
@@ -34,11 +38,13 @@ class MainActivity :AppCompatActivity() {
 
     private fun bmiInfo (bmi: Float): String {
         return when (bmi) {
-            in 0f..15f -> "Mega lipa! Chudzielec!!"
-            in 16f..17f -> "Chudzinka"
-            in 18f..18.5f -> "Normalna chudzina"
-            in 19f..25f -> "Szupła osoba"
-            else -> "Otyłość"
+            in 0f..15.99f -> "Wychudzenie!! Zyjesz jeszcze? :)"
+            in 16f..17.99f -> "Wygłodzenie - czas by odwiedzić babcię!! "
+            in 18f..18.49f -> "Niedowaga - jeden obiadek więcej nie zaszkodzi "
+            in 18.5f..24.99f -> "Wszystko w normie! Tak trzymaj!!"
+            in 25f..29.99f -> "Nadwaga! Teraz obiadek mniej!"
+            in 30f..34.99f -> "Otyłość! Wypada pomyśleć nad dietą"
+            else -> "Otyłość klinicza!! Caasu udać się do lekarza!!!"
         }
 /*        return if (bmi < 16) {
             "Mega lipa! Chudzielec!!"
@@ -58,6 +64,5 @@ class MainActivity :AppCompatActivity() {
     1. utworzyc 3 xml'e - 1 fragment_splash, 2 - fragment_main (to samo co w activityMain - nic nie ruszaj!), 3 fragment_result (Prezentacja wyników)
     2. utworz 3 fragmenty i te fragmenty mają używać viewbinding
     3. Poczytac o navigation component
-    4. Przeniesc ekran z Bmi do fragmentu
      */
 }
