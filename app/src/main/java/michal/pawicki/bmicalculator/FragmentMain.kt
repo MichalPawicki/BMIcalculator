506f10760c77ddc4fee7bcf27a68b50c1362f859
MichalPawicki <michal_pawicki@op.pl>
2021-10-05 19:26:47 +0200

    Safe args and calculate Bmi

--- app/src/main/java/michal/pawicki/bmicalculator/FragmentMain.kt
+++ app/src/main/java/michal/pawicki/bmicalculator/FragmentMain.kt
@@ -28,6 +28,37 @@ class FragmentMain : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonCalculate.setOnClickListener {
+
+            val height = binding.addHeight.text.toString().toFloat()
+            if (height == 0f ) {
+                binding.addHeight.error = "Podaj wartość większą od 0"
+                return@setOnClickListener
+            }
+            val weight = binding.addWeight.text.toString().toFloat()
+            if (weight == 0f) {
+                binding.addWeight.error = "Podaj wartość większą od 0"
+                return@setOnClickListener
+            }
+            val bmi = calculateBmi(height, weight)
+//            binding.textview.text = "Twoja wartość BMI wynosi: $bmi"
+//            binding.bmiInfo.text = bmiInfo(bmi)
+        }
+
+    }
+
+    private fun calculateBmi (height: Float, weight: Float) = weight/(height*height)
+
+    private fun bmiInfo (bmi: Float): String {
+        return when (bmi) {
+            in 0f..15.99f -> "Wychudzenie!! Zyjesz jeszcze? :)"
+            in 16f..17.99f -> "Wygłodzenie - czas by odwiedzić babcię!! "
+            in 18f..18.49f -> "Niedowaga - jeden obiadek więcej nie zaszkodzi "
+            in 18.5f..24.99f -> "Wszystko w normie! Tak trzymaj!!"
+            in 25f..29.99f -> "Nadwaga! Teraz obiadek mniej!"
+            in 30f..34.99f -> "Otyłość! Wypada pomyśleć nad dietą"
+            else -> "Otyłość klinicza!! Caasu udać się do lekarza!!!"
+        }
+
             findNavController().navigate(R.id.action_fragmentMain_to_fragmentResult)
         }
     }
