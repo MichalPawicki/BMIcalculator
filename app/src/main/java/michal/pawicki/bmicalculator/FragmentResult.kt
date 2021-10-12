3ca9e05b2e7e3502fa9d99469a6d53e322f4464f
MichalPawicki <michal_pawicki@op.pl>
2021-10-12 17:10:45 +0200

    corrects

--- app/src/main/java/michal/pawicki/bmicalculator/FragmentResult.kt
+++ app/src/main/java/michal/pawicki/bmicalculator/FragmentResult.kt
@@ -1,11 +1,9 @@
 package michal.pawicki.bmicalculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import michal.pawicki.bmicalculator.databinding.FragmentResultBinding
@@ -34,18 +32,19 @@ class FragmentResult : Fragment (R.layout.fragment_result) {
 
         binding.textViewFragResult.text = "Twoja wartość BMI wynosi: ${args.textargs}"
 
-        //        binding.bmiInfo.text = bmiInfo(bmi)
+        val bmi = args.textargs
+        binding.bmiInfo.text = bmiInfo(bmi)
     }
-//    private fun bmiInfo (bmi: Float): String {
-//        return when (bmi) {
-//            in 0f..15.99f -> "Wychudzenie!! Zyjesz jeszcze? :)"
-//            in 16f..17.99f -> "Wygłodzenie - czas by odwiedzić babcię!! "
-//            in 18f..18.49f -> "Niedowaga - jeden obiadek więcej nie zaszkodzi "
-//            in 18.5f..24.99f -> "Wszystko w normie! Tak trzymaj!!"
-//            in 25f..29.99f -> "Nadwaga! Teraz obiadek mniej!"
-//            in 30f..34.99f -> "Otyłość! Wypada pomyśleć nad dietą"
-//            else -> "Otyłość klinicza!! Caasu udać się do lekarza!!!"
-//        }
-//    }
+    private fun bmiInfo (bmi: String): String {
+        return when (bmi.toFloat()) {
+            in 0f..15.99f -> "Wychudzenie!! Zyjesz jeszcze? :)"
+            in 16f..17.99f -> "Wygłodzenie - czas by odwiedzić babcię!! "
+            in 18f..18.49f -> "Niedowaga - jeden obiadek więcej nie zaszkodzi "
+            in 18.5f..24.99f -> "Wszystko w normie! Tak trzymaj!!"
+            in 25f..29.99f -> "Nadwaga! Teraz obiadek mniej!"
+            in 30f..34.99f -> "Otyłość! Wypada pomyśleć nad dietą"
+            else -> "Otyłość klinicza!! Caasu udać się do lekarza!!!"
+        }
+    }
 
 }
